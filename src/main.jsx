d18d5a4dfa57373985d6e6fca54cd0b759767bd6
kwangric <43795498+kwangric@users.noreply.github.com>
2022-06-26 14:01:50 -0400

    Fixed ReactDOM issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './components/store'
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
